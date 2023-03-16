03192cf23a47b296c1e5987a707262dca3dcf8bd
Matt Oake <matt@Matts-MacBook-Air.local>
2023-03-16 16:28:45 -0300

    test onClick, style hover pointer

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -2,14 +2,18 @@ import React from "react";
 import reactLogo from "../img/reactLogo.png";
 import "./GameCard.css";
 
-function clickCard() {
-  console.log("clicked");
+function clickCard(cardValue) {
+  console.log(cardValue);
 }
 
 export default function GameCard(props) {
   return (
     <div className="gameCard">
-      <img src={reactLogo} onClick={clickCard} width="200px"></img>
+      <img
+        src={reactLogo}
+        onClick={() => clickCard(props.face)}
+        width="200px"
+      ></img>
     </div>
   );
 }
