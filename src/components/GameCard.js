931c39a746400e008a1093d606c24eb1232f25ab
Matt Oake <matt@Matts-MacBook-Air.local>
2023-03-16 16:37:07 -0300

    state variable for weather card was clicked or not

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import reactLogo from "../img/reactLogo.png";
 import "./GameCard.css";
 
@@ -7,11 +7,15 @@ function clickCard(cardValue) {
 }
 
 export default function GameCard(props) {
+  const [clicked, setClicked] = useState(false);
+  if (clicked) {
+    return <h1 onClick={() => setClicked(!clicked)}>WORD</h1>;
+  }
   return (
     <div className="gameCard">
       <img
         src={reactLogo}
-        onClick={() => clickCard(props.face)}
+        onClick={() => setClicked(!clicked)}
         width="200px"
       ></img>
     </div>
