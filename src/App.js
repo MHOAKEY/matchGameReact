81eb8f9ac28d6062996bd6630b701aea7b567d04
Matt Oake <matt@Matts-MacBook-Air.local>
2023-03-16 16:05:27 -0300

    changed className for gaem area, style images on hover to have a spin effect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import "./App.css";
 import reactLogo from "./img/reactLogo.png";
 
+function clickCard() {
+  console.log("clicked");
+}
+
 function App() {
   return (
     <div>
       <h1>MATCH GAME</h1>
       <h3>How quick can you match them all?!</h3>
       <div className="gameArea">
-        <img src={reactLogo}></img>
-        <img src={reactLogo}></img>
-        <img src={reactLogo}></img>
-        <img src={reactLogo}></img>
-        <img src={reactLogo}></img>
-        <img src={reactLogo}></img>
-        <img src={reactLogo}></img>
-        <img src={reactLogo}></img>
-        <img src={reactLogo}></img>
-        <img src={reactLogo}></img>
-        <img src={reactLogo}></img>
-        <img src={reactLogo}></img>
+        <img src={reactLogo} onClick={clickCard}></img>
+        <img src={reactLogo} onClick={clickCard}></img>
+        <img src={reactLogo} onClick={clickCard}></img>
+        <img src={reactLogo} onClick={clickCard}></img>
+        <img src={reactLogo} onClick={clickCard}></img>
+        <img src={reactLogo} onClick={clickCard}></img>
+        <img src={reactLogo} onClick={clickCard}></img>
+        <img src={reactLogo} onClick={clickCard}></img>
+        <img src={reactLogo} onClick={clickCard}></img>
+        <img src={reactLogo} onClick={clickCard}></img>
+        <img src={reactLogo} onClick={clickCard}></img>
+        <img src={reactLogo} onClick={clickCard}></img>
       </div>
       <div></div>
     </div>
