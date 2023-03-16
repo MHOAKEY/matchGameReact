f0a45c3c50eb97689801e492a154523016e13046
Matt Oake <matt@Matts-MacBook-Air.local>
2023-03-16 15:57:12 -0300

    started layin gout basic design, style for images (flex)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import reactLogo from "./img/reactLogo.png";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <h1>MATCH GAME</h1>
+      <h3>How quick can you match them all?!</h3>
+      <div className="gameArea">
+        <img src={reactLogo}></img>
+        <img src={reactLogo}></img>
+        <img src={reactLogo}></img>
+        <img src={reactLogo}></img>
+        <img src={reactLogo}></img>
+        <img src={reactLogo}></img>
+        <img src={reactLogo}></img>
+        <img src={reactLogo}></img>
+        <img src={reactLogo}></img>
+        <img src={reactLogo}></img>
+        <img src={reactLogo}></img>
+        <img src={reactLogo}></img>
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
